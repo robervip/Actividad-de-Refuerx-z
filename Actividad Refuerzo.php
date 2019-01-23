2ec21d7631eeb99fd8021828daba1074146e5637
robervip <44229332+robervip@users.noreply.github.com>
2019-01-23 12:00:07 +0100

    01

---
+++ Actividad Refuerzo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Actividad de Refuerzo 01</title>
+  </head>
+  <body>
+    <ul>
+    <?php
+    for ($i=1; $i < 41; $i=$i+2) {
+      echo "<li>$i</li>";
+    }
+     ?>
+   </ul>
+  </body>
+</html>
